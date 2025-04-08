1dbd6018fea9c3bbb1330f669b3c8505b1dc1263
Caique <caiquedebritoo@gmail.com>
2025-04-08 12:21:20 -0300

    implement JWT authentication in order gateway; validate token on connection and restrict order status updates to chefs

--- src/order/order.gateway.ts
+++ src/order/order.gateway.ts
@@ -1,21 +1,40 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderService } from './order.service';
+import { JwtService } from '@nestjs/jwt';
+import { UnauthorizedException } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
-    oringin:'*',
+    origin:'*',
   }
 })
 export class OrderGateway {
   @WebSocketServer() 
   server: Server;
 
-  constructor(private readonly orderService: OrderService) {}
+  constructor(
+    private readonly orderService: OrderService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async handleConnection(client: Socket) {
-    console.log('client connected');
+    const token = client.handshake.auth?.token.split(' ')[1];
+
+    if (!token) {
+      throw new UnauthorizedException('Token not provided');
+    }
+
+    try {
+      const decoded = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      client.data.user = decoded;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+
     const orders = await this.orderService.getOrders();
     this.server.emit('orders', orders);
   }
@@ -27,7 +46,13 @@ export class OrderGateway {
   }
 
   @SubscribeMessage('updateOrderStatus')
-  async handleUpdateOrderStatus(@MessageBody() data: { orderId: string, status: string }) {
+  async handleUpdateOrderStatus(@MessageBody() data: { orderId: string, status: string }, @ConnectedSocket() client: Socket) {
+    const user = client.data.user;
+
+    if (user.type !== 'CHEF') {
+      throw new UnauthorizedException('Only chefs can update order status');
+    }
+
     const updatedOrder = await this.orderService.updateOrder(data.orderId, data.status);
     this.server.emit('updateOrderStatus', updatedOrder);
   }
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { OrderGateway } from "./order.gateway";
 import { PrismaService } from "src/prisma.service";
 import { OrderService } from "./order.service";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
+  imports: [JwtModule],
   providers: [OrderGateway, PrismaService, OrderService],
 })
 export class OrderModule {}
